f7253e8176a9353de7676a75de1b8f602ad053bd
Mikenzie Ginsberg <mginsberg24@gmail.com>
2014-06-14 22:12:01 +0200

    fixed account balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   # validates :fname, :lname, presence: true
   validates :email, presence: true, uniqueness: true
+  #validetes :uid, uniqueness: true
+
+  after_create :create_account
 
   def self.from_omniauth(auth)
-    User.where(auth.slice(:uid)).first_or_create do |user|
+    user = User.where(auth.slice(:uid)).first_or_create do |user|
       user.uid = auth.uid
       user.fname = auth.info.first_name
       user.email = auth.info.email
       user.lname = auth.info.last_name
       user.profile_pic = auth.extra.raw_info.profile_picture_url
-      user.token = auth.credentials.token
-
-      account = user.account || Account.create(:user_id => user.id)
-      # account.balance = auth.info.balance
-      account.save  
+      user.token = auth.credentials.token     
     end
   end
 
@@ -34,6 +33,10 @@ class User < ActiveRecord::Base
 	  end    
 	end
 
+  protected
 
+  def create_account
+    Account.create!(:user_id => self.id, :balance => 500)
+  end
 
 end
