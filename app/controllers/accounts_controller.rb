93045a9bc22a5150de4219320510794e4db2f26a
Brandon Taleisnik <btaleisnik@gmail.com>
2014-06-18 00:57:45 +0200

    added modal popup for new user account setup, gave dashboard a framework

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,25 @@
-class AccountController < ApplicationController
+class AccountsController < ApplicationController
  
-  def edit
-  end
+def edit
+end
+
 
   def update
+  	@account = current_user.account
+
+	@account.update_attributes(account_params)
+  	redirect_to root_path
   end
+
 
   def show
   end
+
+  private
+
+  def account_params
+		params.require(:account).permit(:collateral, :lending_funds)
+	end
 end
+
+
